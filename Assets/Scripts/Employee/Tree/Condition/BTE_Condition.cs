8e3267349b57978253a775fbccb9f3d6d7258fd5
pisroune <60748451+pisroune@users.noreply.github.com>
2020-09-27 18:18:33 +0800

    0.001

---
+++ Assets/Scripts/Employee/Tree/Condition/BTE_Condition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class BTE_Condition : Conditional
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Employee/Tree/Condition/BTE_MoveFlag.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+/// <summary>
+/// 移动信号
+/// </summary>
+public class BTE_MoveFlag : Conditional
+{
+    public SharedBool MoveFlag;
+
+
+}
