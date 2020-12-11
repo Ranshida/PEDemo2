4a84ef7b73190510c371cc3e9e82312556e3ef6b
pisroune <pisroune@163.com>
2020-12-11 14:05:25 +0800

    0.14444_4
    Bug修正：
    ·修复事件对象报空的Bug

--- Assets/Scripts/Employee/BehaviourTree/Action/BTE_WaitEvent.cs
+++ Assets/Scripts/Employee/BehaviourTree/Action/BTE_WaitEvent.cs
@@ -29,7 +29,9 @@ public class BTE_WaitEvent : BTE_Action
             //独立事件 => 直接执行
             if (!ThisEntity.CurrentEvent.HaveTarget)
             {
+                Debug.LogError(ThisEntity.name + "Self");
                 ThisEntity.SolveEvent();
+                return TaskStatus.Running;
             }
 
             //主动发起方去寻找被动方 => 找到后执行
@@ -41,7 +43,11 @@ public class BTE_WaitEvent : BTE_Action
 
                 //满足开始执行事件的条件（TODO）
                 if (Function.XZDistance(ThisEntity.transform.position, Destination.Value) < 8)
+                {
+                    Debug.LogError(ThisEntity.name + "Target");
                     ThisEntity.SolveEvent();
+                    return TaskStatus.Running;
+                }
             }
         }
 
