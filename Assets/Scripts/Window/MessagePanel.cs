63e7c290d5156c2d598e5963a6ab46366c66c607
pisroune <pisroune@163.com>
2021-01-09 17:41:39 +0800

    0.14445_43M
    新添部分：
    ·弹窗提示
    ·任务引导功能

--- Assets/Scripts/Window/MessagePanel.cs
+++ Assets/Scripts/Window/MessagePanel.cs
@@ -2,12 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MessagePanel : MonoBehaviour
 {
-    string Text;
-    Action OnAccept;
-    Action OnRefuse;
+    public Transform typeA;
+    public Transform typeB;
+
+    string m_Text;
+    Action m_OnAccept;
+    Action m_OnRefuse;
 
     /// <summary>
     /// 确定，确定之后接触暂停继续
@@ -15,7 +19,26 @@ public class MessagePanel : MonoBehaviour
     /// <param name="txt">文案</param>
     public void Init(string txt)
     {
+        typeA.gameObject.SetActive(true);
+        GameControl.Instance.AskPause(this);
 
+        string[] str = txt.Split('|');
+        if (str.Length == 2)
+        {
+            typeA.transform.Find("Txt_Title").GetComponent<Text>().text = str[0];
+            typeA.transform.Find("Txt_Info").GetComponent<Text>().text = str[1];
+        }
+        else
+        {
+            typeA.transform.Find("Txt_Info").GetComponent<Text>().text = txt;
+        }
+
+        typeA.transform.Find("Btn_Accept").GetComponent<Button>().onClick.RemoveAllListeners();
+        typeA.transform.Find("Btn_Accept").GetComponent<Button>().onClick.AddListener(()=>
+        {
+            GameControl.Instance.RemovePause(this);
+            Destroy(gameObject);
+        });
     }
 
     /// <summary>
@@ -25,7 +48,27 @@ public class MessagePanel : MonoBehaviour
     /// <param name="onAccept">确定后执行</param>
     public void Init(string txt, Action onAccept)
     {
+        typeA.gameObject.SetActive(true);
+        GameControl.Instance.AskPause(this);
 
+        string[] str = txt.Split('|');
+        if (str.Length == 2)
+        {
+            typeA.transform.Find("Txt_Title").GetComponent<Text>().text = str[0];
+            typeA.transform.Find("Txt_Info").GetComponent<Text>().text = str[1];
+        }
+        else
+        {
+            typeA.transform.Find("Txt_Info").GetComponent<Text>().text = txt;
+        }
+
+        typeA.transform.Find("Btn_Accept").GetComponent<Button>().onClick.RemoveAllListeners();
+        typeA.transform.Find("Btn_Accept").GetComponent<Button>().onClick.AddListener(() =>
+        {
+            onAccept();
+            GameControl.Instance.RemovePause(this);
+            Destroy(gameObject);
+        });
     }
 
     /// <summary>
@@ -36,8 +79,34 @@ public class MessagePanel : MonoBehaviour
     /// <param name="onRefuse">拒绝后执行</param>
     public void Init(string txt, Action onAccept, Action onRefuse)
     {
-        Text = txt;
-        OnAccept = onAccept;
-        OnRefuse = onRefuse;
+        typeB.gameObject.SetActive(true);
+        GameControl.Instance.AskPause(this);
+
+        string[] str = txt.Split('|');
+        if (str.Length == 2)
+        {
+            typeB.transform.Find("Txt_Title").GetComponent<Text>().text = str[0];
+            typeB.transform.Find("Txt_Info").GetComponent<Text>().text = str[1];
+        }
+        else
+        {
+            typeB.transform.Find("Txt_Info").GetComponent<Text>().text = txt;
+        }
+
+        typeB.transform.Find("Btn_Accept").GetComponent<Button>().onClick.RemoveAllListeners();
+        typeB.transform.Find("Btn_Refuse").GetComponent<Button>().onClick.RemoveAllListeners();
+        typeB.transform.Find("Btn_Accept").GetComponent<Button>().onClick.AddListener(() =>
+        {
+            onAccept();
+            GameControl.Instance.RemovePause(this);
+            Destroy(gameObject);
+        });
+        
+        typeB.transform.Find("Btn_Refuse").GetComponent<Button>().onClick.AddListener(() =>
+        {
+            onRefuse();
+            GameControl.Instance.RemovePause(this);
+            Destroy(gameObject);
+        });
     }
 }
