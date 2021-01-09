c11edf3b95d2a478e94dfbbe5dcc2888532458b3
pisroune <pisroune@163.com>
2021-01-09 15:37:37 +0800

    0.14445_43M
    新添部分：
    ·弹窗的几个接口
    ·完成任务引导部分的接口

--- Assets/Scripts/QuestControl.cs
+++ Assets/Scripts/QuestControl.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class QuestControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static QuestControl Instance;
+
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 确定，确定之后接触暂停继续
+    /// </summary>
+    /// <param name="txt">文案</param>
+    public void Init(string txt)
     {
-        
+
+    }
+
+    /// <summary>
+    /// 确定，确定之后执行方法
+    /// </summary>
+    /// <param name="txt">文案</param>
+    /// <param name="onAccept">确定后执行</param>
+    public void Init(string txt, Action onAccept)
+    {
+
+    }
+
+    /// <summary>
+    /// 接受或拒绝
+    /// </summary>
+    /// <param name="txt">文案</param>
+    /// <param name="onAccept">接受后执行</param>
+    /// <param name="onRefuse">拒绝后执行</param>
+    public void Init(string txt, Action onAccept, Action onRefuse)
+    {
+
+    }
+
+    /// <summary>
+    /// 引导任务
+    /// </summary>
+    /// <param name="step">第几个任务</param>
+    public void Finish(int step)
+    {
+
     }
 }
---
+++ Assets/Scripts/Window/MessagePanel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessagePanel : MonoBehaviour
+{
+    string Text;
+    Action OnAccept;
+    Action OnRefuse;
+
+    /// <summary>
+    /// 确定，确定之后接触暂停继续
+    /// </summary>
+    /// <param name="txt">文案</param>
+    public void Init(string txt)
+    {
+
+    }
+
+    /// <summary>
+    /// 确定，确定之后执行方法
+    /// </summary>
+    /// <param name="txt">文案</param>
+    /// <param name="onAccept">确定后执行</param>
+    public void Init(string txt, Action onAccept)
+    {
+
+    }
+
+    /// <summary>
+    /// 接受或拒绝
+    /// </summary>
+    /// <param name="txt">文案</param>
+    /// <param name="onAccept">接受后执行</param>
+    /// <param name="onRefuse">拒绝后执行</param>
+    public void Init(string txt, Action onAccept, Action onRefuse)
+    {
+        Text = txt;
+        OnAccept = onAccept;
+        OnRefuse = onRefuse;
+    }
+}
